886f7e81156fd5c0509fa55b3f64dbbda30ec8dd
DanLucena <danlucas1711@gmail.com>
2021-06-14 21:15:06 -0300

    Primeiro commit, consertando packet json

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -1,24 +1,10 @@
-import { SignInButton } from './SignInButton'
 import styles from './styles.module.scss'
-import {ActiveLink} from '../ActiveLink'
-export function Header(){
+
+export function Header() {
 
     return (
-        <header className={styles.headerContainer}>
-            <div className = {styles.headerContent}>
-                <img src="/images/logo.svg" alt="Ig News" />
-                <nav>
-                    <ActiveLink activeClass={styles.active} href='/'>
-                        <a>Home</a>
-                    </ActiveLink>
-                    <ActiveLink activeClass={styles.active} href='/posts'>
-                        <a>Posts</a>
-                    </ActiveLink>
-                    
-                    
-                </nav>
-                <SignInButton />
-            </div>
-        </header>
+        <>
+            hello worldasdfasdf
+        </>
     );
 }
