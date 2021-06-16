af0f9ef24d92dcb051ff4f696d5d595c08aa383b
Gabriel Carvalho <braga.gabrielcarvalho@gmail.com>
2021-06-16 11:21:33 -0300

    Renamed listagem to problems and added a SSG strategy thats getting posts from Ghost CMS

---
+++ src/pages/problems/index.tsx
@@ -0,0 +1,39 @@
+import styles from "./styles.module.scss";
+import { ProblemCard } from "../../components/ProblemCard";
+import { GetStaticProps } from "next";
+import { getPosts } from "../../services/ghost";
+
+interface Problem {
+  slug: string;
+  title: string;
+  excerpt: string;
+}
+
+interface ProblemsProps {
+  problems: Problem[];
+}
+
+export default function ProblemsList({ problems }: ProblemsProps) {
+  return (
+    <div className={styles.listProblemsContent}>
+      <div className={styles.listProblemContainer}>
+        {problems.map((problem) => (
+          <ProblemCard
+            title={problem.title}
+            description={problem.excerpt}
+            slug={problem.slug}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const problems = await getPosts();
+
+  return {
+    props: { problems },
+    revalidate: 60 * 5, // 5 minutes
+  };
+};
